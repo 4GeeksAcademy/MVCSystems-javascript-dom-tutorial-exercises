4a51f3944b951f49d0bfc29bcadce6fd91a4055c
MVCSystems <carlosmerav@gmail.com>
2024-08-04 01:04:27 -0500

    Add

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -2,6 +2,6 @@ let button = document.getElementById("superDuperButton");
 button.addEventListener("click", function() {
 	// Your code here
 	let li = document.createElement("li");
-	li.innerHTML = "New Item";
+	li.innerHTML = "Fourth element";
 	document.querySelector("#myList").appendChild(li);
 });
